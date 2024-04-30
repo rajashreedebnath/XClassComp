b7d816ebdd6829500932a265d1c2ce12e975e8dc
rajashreedebnath <rajashreedebnath29@gmail.com>
2024-04-30 05:57:24 +0530

    Test

--- class_comp/src/App.js
+++ class_comp/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { Component, useState } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class Counter extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+
+  handleIncrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  render() {
+
+    const { count } = this.state;
+
+
+
+    return (
+      <div className="counter">
+
+        <h1> Counter App </h1>
+  
+        <div className="count">
+          Count: {count}
+        </div>
+  
+        <div className="buttons">
+  
+          <button onClick={this.handleIncrement}> Increment </button>
+  
+          <button onClick={this.handleDecrement}> Decrement </button>
+  
+        </div>
+        
+      </div>
+    );
+
+  }
+
+
+}
